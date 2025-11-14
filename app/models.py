76d7574d80d8a2205180d4e7814afe84ccf21700
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2025-11-14 10:13:50 +0000

    resolve mypy issue

--- app/models.py
+++ app/models.py
@@ -1,11 +1,17 @@
 import uuid
+from typing import TYPE_CHECKING
 
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import Mapped, mapped_column
 
 from app import db
 
+if TYPE_CHECKING:
+    from flask_sqlalchemy.model import Model
+else:
+    Model = db.Model
 
-class Register(db.Model):
+
+class Register(Model):
     id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     name: Mapped[str] = mapped_column(nullable=False, unique=True, index=True)
