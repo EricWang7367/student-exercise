dd6bc1d6c381734b7a7c2d1287e0caaaa5c505ba
Dan Cottam <dan.cottam@landregistry.gov.uk>
2025-11-23 21:17:56 +0000

    Running black

--- truncate_db.py
+++ truncate_db.py
@@ -2,10 +2,11 @@ import argparse
 
 import psycopg2
 
+
 def truncate(database, user, password, host, port):
     try:
-        with  psycopg2.connect(database=database, user=user, password=password, host=host, port=port) as conn:
-            with  conn.cursor() as cur:
+        with psycopg2.connect(database=database, user=user, password=password, host=host, port=port) as conn:
+            with conn.cursor() as cur:
                 cur.execute("TRUNCATE register CASCADE")
                 rows_deleted = cur.rowcount
             # commit the changes to the database
@@ -14,12 +15,13 @@ def truncate(database, user, password, host, port):
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Truncates the local database')
-    parser.add_argument('--host', type=str, default='localhost')
-    parser.add_argument('--port', type=int, default=5432)
-    parser.add_argument('--database_name', type=str, default='db')
-    parser.add_argument('-u', '--user', type=str, required=True)
-    parser.add_argument('-p', '--password', type=str, required=True)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Truncates the local database")
+    parser.add_argument("--host", type=str, default="localhost")
+    parser.add_argument("--port", type=int, default=5432)
+    parser.add_argument("--database_name", type=str, default="db")
+    parser.add_argument("-u", "--user", type=str, required=True)
+    parser.add_argument("-p", "--password", type=str, required=True)
     args = parser.parse_args()
     truncate(database=args.database_name, user=args.user, password=args.password, host=args.host, port=args.port)
